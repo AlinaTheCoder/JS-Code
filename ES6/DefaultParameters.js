df2eb982a268dde21f6a0485e784a0657e51e0c3
AlinaTheCoder <alinalearns6@gmail.com>
2024-08-04 09:10:21 +0000

    Default Parameters in JS

--- ES6/DefaultParameters.js
+++ ES6/DefaultParameters.js
@@ -1,1 +1,7 @@
-console.log("Hello World!.");
+// if you don't define/value the variables just declare them them by default there value is undefined
+
+function guestUser(name = "unKnown", courseCount = 0) {
+    console.log(`Hello ${name}, You're registered in ${courseCount} courses.`)
+}
+// guestUser(); 
+guestUser("Alina", 1);

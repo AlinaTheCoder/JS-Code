e0d6f26a18505fae6b29080915ea81d95c45c906
AlinaTheCoder <alinalearns6@gmail.com>
2024-08-06 07:34:16 +0000

    Extract Specific elements from bunce (array/object)

--- ES6/Destructing.js
+++ ES6/Destructing.js
@@ -1,33 +1,37 @@
 let myArray = [3, 4, 8, 5, 1, 34, 8, 12];
-/* let a = myArray[0];
+
+/*
+// Basic array destructuring
+let a = myArray[0];
 let b = myArray[1];
-let [a,b] = myArray;
-console.log(a);
-console.log(b);
+let [a, b] = myArray;
+console.log(a); // Outputs: 3
+console.log(b); // Outputs: 4
 */
 
-// array destructing works based on the  particular index positions
-/* let [, b, , d] = myArray;
-console.log(b);
-console.log(d); 
-let [, b, , ...args] = myArray; //...args working as the spread operator
-console.log(b);
-console.log(args);
+// Array destructuring based on index positions
+/*
+let [, b, , d] = myArray;
+console.log(b); // Outputs: 4
+console.log(d); // Outputs: 5
+let [, b, , ...args] = myArray; // ...args works as the rest operator
+console.log(b); // Outputs: 4
+console.log(args); // Outputs: [5, 1, 34, 8, 12]
 */
 
-// Object Destructing -> based on the key/property names
-let person ={
-    id:1,
-    name:"Alina",
-    age:30,
-    address:{
-        country:"Pakistan",
-        state:"Gujrat, Punjab"
+// Object destructuring based on key/property names
+let person = {
+    id: 1,
+    name: "Alina",
+    age: 21,
+    address: {
+        country: "Pakistan",
+        state: "Gujrat, Punjab"
     }
-}
+};
 
-let {id,name:firstName,address:{country}} = person;
-console.log(id);
-console.log(firstName);
-// console.log(address);
-console.log(country);
+let { id, name: firstName, address: { country } } = person;
+console.log(id); // Outputs: 1
+console.log(firstName); // Outputs: Alina
+// console.log(address); // Uncomment to output the address object
+console.log(country); // Outputs: Pakistan
